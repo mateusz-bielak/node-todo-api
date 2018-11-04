96d4c85da72bd549dc6ea3bbd77650b9326aed6f
Mateusz Bielak <mateusz.bielak@skygate.io>
2018-11-04 17:35:40 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { mongoose } = require('./db/mongoose');
+require('./db/mongoose');
 const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    const { send } = res;
     const todo = new Todo({
         text: req.body.text,
     });
@@ -19,6 +17,15 @@ app.post('/todos', (req, res) => {
         .then(doc => res.send(doc))
         .catch(error => res.status(400).send(error));
 });
+
+app.get('/todos', (req, res) =>
+    Todo.find()
+        .then(todos => res.send({ todos }))
+        .catch(e => {
+            console.log(e);
+            res.status(400).send(e);
+        }),
+);
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
