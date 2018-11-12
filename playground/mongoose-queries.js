668c575cc6b1211072e9698abe7fe85eafd123e8
Mateusz Bielak <mateusz.bielak@skygate.io>
2018-11-12 20:08:11 +0100

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,27 @@
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('../server/db/mongoose');
+const { Todo } = require('../server/models/todo');
+
+const id = '5bdf251b588b726cad3ae275';
+
+if (!ObjectID.isValid(id)) {
+    console.log('ID not valid');
+}
+
+// Todo.find({
+//     _id: id,
+// }).then(console.log);
+
+// Todo.findOne({
+//     _id: id,
+// }).then(console.log);
+
+Todo.findById(id)
+    .then(todo => {
+        if (!todo) {
+            return console.log('Id not found');
+        }
+
+        console.log(todo);
+    })
+    .catch(e => console.log(e));
